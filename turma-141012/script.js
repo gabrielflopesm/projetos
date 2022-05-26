f5fbfd8989ec929f238fb4e85005199c1be209d8
gabrielflopesm <gabrielflopesm@outlook.com>
2022-05-26 13:19:33 -0300

    Inserindo um script para alterar as páginas de estilo do documento

---
+++ turma-141012/script.js
@@ -0,0 +1,15 @@
+function noStyles() {
+    document.styleSheets[0].disabled = true;
+    document.styleSheets[1].disabled = true;
+    document.styleSheets[2].disabled = true;
+    document.styleSheets[3].disabled = true;
+
+}
+
+function reStyle(n) {
+    noStyles();
+    document.styleSheets[n].disabled = false;
+
+}
+
+reStyle(0);
